f9964d5a8f77a735d4e4cb5e49d5b29ca6ba95fe
Kinoshita Kiyoshi Nicklaus <nick@Kinoshitas-Mac-mini.local>
2025-06-13 23:16:08 +0800

    Completed initial code.

--- Arduino_nano_project1.ino
+++ Arduino_nano_project1.ino
@@ -2,13 +2,24 @@
 #include <Adafruit_GFX.h> // Graphics functions (text, shapes, bitmaps)
 #include <Adafruit_SSD1306.h> // OLED control functions
 
-#define SCREEN_WIDTH 128
+// Defining global variables
+#define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1 // Resets pin not used on Nano
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Declaring an object
 
 void setup() {
-  // put your setup code here, to run once:
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColour(SSD1306_WHITE);
+  display.setCursor(0, 20);
+  display.print("I ");
+  display.print("<3 ");
+  display.print("You");
+
+  display.display();
 
 }
 
